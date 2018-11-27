45eb96f152b5bf580a5208d8e22b635e51f2b774
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2018-11-27 12:09:48 +0100

    Implemented dynamic change of base href in angular

--- TrafficControlDocs/src/app/app.component.ts
+++ TrafficControlDocs/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  constructor(@Inject(DOCUMENT) private document){
+    let base = this.document.getElementsByTagName('base');
+
+    if(base.length > 0){
+      base[0].setAttribute('href', environment.baseHref)
+    }
+  }
 }
